66c2354b35803f9d4785993d3b95c81295eda5f1
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-04 21:35:52 +0100

    added functions to start game

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+console.log('Started')
+
+function startGame () {
+
+}
+
+function setNextQuestion () {
+
+}
+
+function selectAnswer () { 
+
+
+}
+
